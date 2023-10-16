c12b07e7ea70b326fa3b71acfe62456334165de4
Tuminha <83960151+Tuminha@users.noreply.github.com>
2023-10-16 19:57:21 +0200

    This commit includes the creation of the project repository on GitHub, the addition of a README file for project overview, and the initial commit. The project structure has been outlined with a timeline for development. The main.js file has been created, housing the initial JavaScript code for the quiz application. This includes a 'startQuiz' function that manipulates the DOM to transition from introductory content to the quiz form. An event listener has been added to the quiz form to prevent default form submission behavior and retrieve user input values, which are then logged to the console for verification. This commit serves as the foundation for the Periospot_Quizz project, introducing the basic functionality required for the quiz application.

---
+++ README.md
@@ -0,0 +1,62 @@
+# Project Name: Decentralized Professional Network
+
+## Table of Contents
+1. [Introduction](#introduction)
+2. [Features](#features)
+3. [Technology Stack](#technology-stack)
+4. [Getting Started](#getting-started)
+    - [Prerequisites](#prerequisites)
+    - [Installation](#installation)
+5. [API Documentation](#api-documentation)
+6. [Usage & Examples](#usage--examples)
+7. [Testing](#testing)
+8. [Contribution Guidelines](#contribution-guidelines)
+9. [License](#license)
+10. [Acknowledgements](#acknowledgements)
+11. [Contact & Support](#contact--support)
+
+## Introduction
+- Brief overview of the project.
+- Motivation behind the project.
+
+## Features
+- A comprehensive list of the functionalities and features of the project.
+
+## Technology Stack
+- Languages used.
+- Libraries and frameworks.
+- Tools and software.
+
+## Getting Started
+### Prerequisites
+- Software, tools, or libraries that need to be installed.
+- Version requirements if any.
+
+### Installation
+- Step by step guide on how to get a development environment running.
+- Examples for setting up.
+
+## API Documentation
+- If the project provides an API, detail its endpoints, methods, parameters, and expected responses.
+
+## Usage & Examples
+- Detailed examples on how to use the project.
+- Code snippets or screenshots if applicable.
+
+## Testing
+- Information about how to run the tests.
+- Any frameworks or tools used for testing.
+
+## Contribution Guidelines
+- Details on how others can contribute to the project.
+- Code of conduct or standards to be followed.
+
+## License
+- Information about the project's licensing.
+
+## Acknowledgements
+- Credits to contributors, libraries, or tools used.
+
+## Contact & Support
+- Contact information for the team or individual behind the project.
+- Channels for support, feedback, or queries.
---
+++ js/main.js
@@ -0,0 +1,260 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    // Add event listeners for the submit buttons of each question
+    document.querySelector(".container button").addEventListener('click', startQuiz);
+
+    document.querySelector("#question3 input[type='submit']").addEventListener('click', handleThirdQuestionSubmit);
+    document.querySelector("#question4 input[type='submit']").addEventListener('click', handleFourthQuestionSubmit);
+    document.querySelector("#question5 input[type='submit']").addEventListener('click', handleFifthQuestionSubmit);
+    document.querySelector("#question6 input[type='submit']").addEventListener('click', handleSixthQuestionSubmit);
+    document.querySelector("#question7 input[type='submit']").addEventListener('click', handleSeventhQuestionSubmit);
+    document.querySelector("#question8 input[type='submit']").addEventListener('click', handleEighthQuestionSubmit);
+    document.querySelector("#question9 input[type='submit']").addEventListener('click', handleNinthQuestionSubmit);
+
+    function handleThirdQuestionSubmit(e) {
+        console.log('handleThirdQuestionSubmit called');  // Debugging line
+    
+        e.preventDefault();
+    
+        // Gather the user's answer
+        let selectedOptions = Array.from(document.querySelectorAll("#question3 input[type='checkbox']:checked")).map(option => option.value);
+        console.log(`Selected options: ${selectedOptions.join(', ')}`);
+    
+        // Hide the third question
+        document.getElementById("question3").classList.add("hidden");
+    
+        // Show the fourth question
+        showFourthQuestion();
+    }
+    
+    function showFourthQuestion() {
+        console.log('showFourthQuestion called');  // Debugging line
+    
+        document.getElementById("question4").classList.remove("hidden");
+    }
+    
+    function handleFourthQuestionSubmit(e) {
+        e.preventDefault();
+    
+        // Gather the user's answer
+        let selectedOption = document.querySelector("#question4 input[type='radio']:checked").value;
+        console.log(`Selected option: ${selectedOption}`);
+    
+        // Hide the fourth question
+        document.getElementById("question4").classList.add("hidden");
+    
+        // Show the fifth question
+        showFifthQuestion();
+    }
+    
+    function showFifthQuestion() {
+        document.getElementById("question5").classList.remove("hidden");
+    }
+    
+    
+    
+    function showSixthQuestion() {
+        document.getElementById("question6").classList.remove("hidden");
+    }
+    
+    
+    function handleFifthQuestionSubmit(e) {
+        e.preventDefault();
+        
+        // Gather the user's answer
+        let selectedOption = document.querySelector("#question5 input[type='radio']:checked").value;
+        console.log(`Selected option: ${selectedOption}`);
+    
+        // Hide the fifth question
+        document.getElementById("question5").classList.add("hidden");
+    
+        // Show the sixth question
+        showSixthQuestion();
+    }
+    
+    function showSeventhQuestion() {
+        document.getElementById("question7").classList.remove("hidden");
+    }
+    
+    function handleSixthQuestionSubmit(e) {
+        e.preventDefault();
+    
+        // Gather the user's answer
+        let selectedOption = document.querySelector("#question6 input[type='radio']:checked").value;
+        console.log(`Selected option: ${selectedOption}`);
+    
+        // Hide the sixth question
+        document.getElementById("question6").classList.add("hidden");
+    
+        // Show the seventh question
+        showSeventhQuestion();
+    }
+    
+    function showEighthQuestion() {
+        document.getElementById("question8").classList.remove("hidden");
+    }
+    
+    
+    function handleSeventhQuestionSubmit(e) {
+        e.preventDefault();
+    
+        // Gather the user's answer
+        let selectedOptionElement = document.querySelector("#question7 input[type='radio']:checked");
+        if (selectedOptionElement) {
+            let selectedOption = selectedOptionElement.value;
+            console.log(`Selected option: ${selectedOption}`);
+        } else {
+            console.log("No option selected");
+            return;  // Stop the function if no option is selected
+        }
+    
+        // Hide the seventh question
+        document.getElementById("question7").classList.add("hidden");
+    
+        // Show the eighth question
+        showEighthQuestion();
+    }
+    
+    function handleEighthQuestionSubmit(e) {
+        e.preventDefault();
+    
+        // Gather the user's answer
+        let selectedOption = document.querySelector("#question8 input[type='radio']:checked").value;
+        console.log(`Selected option: ${selectedOption}`);
+    
+        // Hide the eighth question
+        document.getElementById("question8").classList.add("hidden");
+    
+        // Show the ninth question
+        showNinthQuestion();
+    }
+
+    function showNinthQuestion() {
+        document.getElementById("question9").classList.remove("hidden");
+    }
+
+    function handleNinthQuestionSubmit(e) {
+        e.preventDefault();
+    
+        // Gather the user's answer
+        let selectedOptionElement = document.querySelector("#question9 input[type='radio']:checked");
+        if (selectedOptionElement) {
+            let selectedOption = selectedOptionElement.value;
+            console.log(`Selected option: ${selectedOption}`);
+        } else {
+            console.log("No option selected");
+            return;  // Stop the function if no option is selected
+        }
+    
+        // Hide the ninth question
+        document.getElementById("question9").classList.add("hidden");
+    
+        // Show the results or the next question
+        // showResults();
+    }
+    
+    // Add click event listeners to the image options in question 7
+    let imageOptions = document.querySelectorAll("#question7 .image-option");
+    imageOptions.forEach(option => {
+        option.addEventListener('click', function() {
+            // Remove the 'selected' class from all image options
+            imageOptions.forEach(opt => opt.classList.remove('selected'));
+    
+            // Add the 'selected' class to the clicked image option
+            this.classList.add('selected');
+    
+            // Uncheck all corresponding radio buttons
+            document.querySelectorAll("#question7 input[type='radio']").forEach(radio => radio.checked = false);
+    
+            // Check the corresponding radio button
+            let radioButtonId = this.getAttribute('data-radio');
+            document.getElementById(radioButtonId).checked = true;
+        });
+    });
+
+    // Add click event listeners to the image options in question 9
+    let imageOptions9 = document.querySelectorAll("#question9 .image-option");
+    imageOptions9.forEach(option => {
+        option.addEventListener('click', function() {
+            // Remove the 'selected' class from all image options
+            imageOptions9.forEach(opt => opt.classList.remove('selected'));
+
+            // Add the 'selected' class to the clicked image option
+            this.classList.add('selected');
+
+            // Uncheck all corresponding radio buttons
+            document.querySelectorAll("#question9 input[type='radio']").forEach(radio => radio.checked = false);
+
+            // Check the corresponding radio button
+            let radioButtonId = this.getAttribute('data-radio');
+            document.getElementById(radioButtonId).checked = true;
+        });
+    });
+    
+    function startQuiz() {
+        // Hide the introductory content
+        document.querySelector(".container").style.display = "none";
+    
+        // Show the form
+        document.getElementById("quizForm").style.display = "block";
+    
+        // Attach the form submission event listener
+        document.getElementById("quizForm").addEventListener("submit", function(event){
+            event.preventDefault();
+            const firstName = document.getElementById("firstName").value;
+            console.log(`First Name: ${firstName}`);
+            // Use the values as required, for example, to personalize the experience
+    
+            // Hide the form
+            document.getElementById("quizForm").style.display = "none";
+    
+            // Show the third question
+            showThirdQuestion(firstName); 
+        });
+    }
+    
+    // When second question (or another event) is done, show the third question
+    function showThirdQuestion(firstName) {
+        document.getElementById("question3").classList.remove("hidden");
+        document.getElementById("userNameDisplay").textContent = firstName;
+    }
+    
+    // When third question (or another event) is done, show the fourth question
+    function showFourthQuestion() {
+        document.getElementById("question4").classList.remove("hidden");
+    }
+    
+    // For the sixth question
+    document.getElementById("optionD6").addEventListener('change', function() {
+        if(this.checked) {
+            alert("Correct answer for Question 6: Micromotion between the implant and the abutment does not affect the MBL");
+        } else {
+            alert("Incorrect answer for Question 6. Try again.");
+        }
+    });
+    
+    // When fourth question (or another event) is done, show the fifth question
+    function showFifthQuestion() {
+        document.getElementById("question5").classList.remove("hidden");
+    }
+    
+    // When sixth question (or another event) is done, show the seventh question
+    function showSeventhQuestion() {
+        document.getElementById("question7").classList.remove("hidden");
+    }
+    
+    // When fifth question (or another event) is done, show the sixth question
+    function showSixthQuestion() {
+        document.getElementById("question6").classList.remove("hidden");
+    }
+    
+    
+    // When seventh question (or another event) is done, show the eighth question
+    function showEighthQuestion() {
+        document.getElementById("question8").classList.remove("hidden");
+    }
+});
+
+    
+    
+
+
